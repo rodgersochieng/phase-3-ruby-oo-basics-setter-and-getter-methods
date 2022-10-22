5cc18d995aeda9d242cede1699aea3c02f3232ae
rodgersochieng <rodger.ochieng@student.moringaschool.com>
2022-10-22 14:51:26 +0300

    completed lab

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,38 @@
+# class Dog
+#     def name = (name)
+#         @name = name
+#     end
+
+#     def name
+#         @name
+#     end
+
+#     def breed =(breed)
+#         @breed = breed
+#     end
+
+#     def breed
+#         @breed
+#     end
+
+#    end
 class Dog
+    #setter
+def name= (name)
+    @name= name
+end
+     #getter
+def name
+    @name
+end
+      #setter
+def breed= (breed)
+    @breed= (breed)
+end
+    #getter
+def breed
+    @breed
+end
 
 end
+
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,36 @@
-class Person
+# class Person
 
+#     def name = (name)
+#         @name = name
+#     end
+
+#     def name
+#         @name
+#     end
+
+#     def job = (job)
+#         @job = job
+#     end
+
+#     def job
+#         @job
+#     end
+
+# end
+class Person
+    def name= (name)
+        @name= name
+    end
+    
+    def name
+        @name
+    end
+
+    def job= (job)
+        @job= job
+    end
+
+    def job
+        @job
+    end
 end
